aeb1e0de4248ef74d934ed86232c26a3d88178ef
U1iz <1708320534@qq.com>
2023-12-22 00:46:10 +0800

    步入正轨

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        asar: true,
+        // icon: './build/icon/favicon_256.ico',
+        nsis: {
+          oneClick: false,
+          allowElevation: false,
+          allowToChangeInstallationDirectory: true,
+          // installerIcon: './build/icon/favicon_512.ico',
+          // installerHeaderIcon: './build/icon/favicon_512.ico'
+        }
+      }
+    }
+  }
 })
